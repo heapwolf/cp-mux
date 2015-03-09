03ad6be341992575b5572325a0ec02ce4ff6efdf
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-03-09 15:52:51 +0100

    use dd to create files and increase to 200 to get EMFILE

--- test/make-data.sh
+++ test/make-data.sh
@@ -1,7 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 COUNTER=0
-  while [ $COUNTER -lt 100 ]; do
-  mkfile -n 20m ./test/read/$COUNTER.data
-  let COUNTER=COUNTER+1
+while [ $COUNTER -lt 200 ]; do
+    OUT=${DIR}/read/$COUNTER.data
+    if [ ! -f  ${OUT} ]; then
+        dd if=/dev/zero of=${DIR}/read/$COUNTER.data bs=5M count=2
+    fi
+    let COUNTER=COUNTER+1
 done
 
